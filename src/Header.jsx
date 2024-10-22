8916db76e3492c94f7f70325c5d6b17533596114
Mary <gretchen7887@gmail.com>
2024-10-22 17:21:33 +0400

    Upload

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Header = ({title}) => {
+  return (
+    <header>
+      <h1 style={{textAlign:'center'}}>{title}</h1>
+    </header>
+  )
+}
+
+
+
+
+export default Header
